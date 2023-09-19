09bdcacd3912b06b5ba57a394e85777e1df40dd7
Jkomba <johnkombah88@gmail.com>
2023-09-19 13:50:08 +0300

    done with assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+// Function expression called divide
+const divide = function(a, b) {
+    return 2000 / 100;
+}
+// Arrow function called square
+const square = (num) => {
+    return num * num;
+}
+
+// Arrow function called add
+const add = (a, b) => {
+    return a + b;
+}
+
+module.exports = { divide, square, add };
